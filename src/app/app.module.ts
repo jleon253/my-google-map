17343efb523a57a064fe03c00fba3f77c02eb5cd
jleon253 <jleon253@gmail.com>
2020-05-05 19:17:16 -0500

    Iniciando proyecto
    Implementando Google Maps Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './modules/material/material.module';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
